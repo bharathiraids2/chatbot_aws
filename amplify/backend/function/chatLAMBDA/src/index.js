0e44aa3d0e245c795d6e8df0dd91eca501f7c895
bharathiraids2 <dsc.bharathi@gmail.com>
2025-06-02 14:17:13 +0530

    changes

--- amplify/backend/function/chatLAMBDA/src/index.js
+++ amplify/backend/function/chatLAMBDA/src/index.js
@@ -1,27 +1,26 @@
-
-
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
+import {
+    BedrockRuntimeClient,
+    InvokeModelCommand
+} from '@aws-sdk/client-bedrock-runtime';
+export const handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
 
-    console.log(event.queryStringParameters.var)
+    console.log(event.queryStringParameters.input)
     let data = "";
-    if (event.queryStringParameters.var.includes("Hello")) {
+    if (event.queryStringParameters.input.includes("hello")) {
         data = "Hi Bharathi !!!"
     }
-    else if (event.queryStringParameters.var.includes("Hi")) {
+    else if (event.queryStringParameters.input.includes("hi")) {
         data = "Hello Bharathi !!!";
 
     }
-    else if (event.queryStringParameters.var.includes("Hi")) {
+    else if (event.queryStringParameters.input.includes("hi")) {
         data = "Hello Bharathi !!!";
 
     }
     else {
-        data = event.queryStringParameters.var
+        data = event.queryStringParameters.input
     }
 
 
